5e9fe9e726b2217ceb0e1980102c32eb9019ebe3
Vanshika Bajaj <vanshikabajaj@Vanshikas-MacBook-Air.local>
2024-12-10 18:56:07 -0500

    resolved

--- tests/strategies.py
+++ tests/strategies.py
@@ -14,5 +14,10 @@ small_floats = floats(min_value=-100, max_value=100, allow_nan=False)
 med_ints = integers(min_value=1, max_value=20)
 
 
+def is_close(x: float, y: float) -> float:
+    """Checks if input values are close"""
+    return (x - y < 1e-2) and (y - x < 1e-2)
+
+
 def assert_close(a: float, b: float) -> None:
-    assert minitorch.operators.is_close(a, b), "Failure x=%f y=%f" % (a, b)
+    assert is_close(a, b), "Failure x=%f y=%f" % (a, b)
